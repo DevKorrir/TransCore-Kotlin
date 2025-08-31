0949d569f6ff8c0898d8ed2e598d99410ec026f3
Aldo Kipyegon <korryr@korryr.korryr>
2025-08-31 12:52:50 +0300

    theme changed

--- app/src/main/java/com/example/transcore/presentation/theme/Color.kt
+++ app/src/main/java/com/example/transcore/presentation/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// TransCore Brand Colors
+val TransCoreBlue = Color(0xFF1565C0)
+val TransCoreBlueVariant = Color(0xFF1976D2)
+val TransCoreTeal = Color(0xFF00695C)
+val TransCoreTealVariant = Color(0xFF00796B)
